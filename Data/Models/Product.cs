57a1950a3fcd1154f2ecf6043872d04bcbf8d028
Tomasz Lenart <t.lenart@gymmanager.io>
2021-01-07 00:51:25 +0100

    Added Product, ProductQuantityChanges models

---
+++ Data/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LashApp.Data.Models
+{
+    public class Product
+    {
+        [Key]
+        [Required]
+        public int ProductId { get; set; }
+        public bool Active { get; set; }
+        public string Name { get; set; }
+        public string Quantity { get; set; }
+        public string Price { get; set; }
+
+    }
+}
---
+++ Data/Models/ProductQuantityChange.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LashApp.Data.Models
+{
+    public class ProductQuantityChange
+    {
+        [Key]
+        [Required]
+        public int ProductQuantityChangeId { get; set; }
+        public bool Active { get; set; }
+       
+        [ForeignKey("ProductId")]
+        [Required]
+        public int LocationId { get; set; }
+        public bool IsPurchase { get; set; }
+        public int QuantityChange { get; set; }
+        public DateTime Date { get; set; }
+
+        public  virtual Product Product { get; set; }
+    }
+}
